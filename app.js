8b89be756270a367771b158c8e42821d5add68be
Pedro <jotahpeee@live.com>
2023-05-31 20:33:38 -0300

    feat: added data to vue

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-Vue.createApp({}).mount('#app')
+Vue.createApp({
+    data() {
+        //gets called when created
+        return {
+            firstName: 'Pedro'
+        }
+    }
+}).mount('#app')
+//what element it controls
+//expressions: evaluates to a single value, 
