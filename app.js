594690689fb94dfa691e82f66d9ca91221dd8f2d
Pedro <jotahpeee@live.com>
2023-05-31 20:48:47 -0300

    feat: added another vue instance to control second app

--- app.js
+++ app.js
@@ -6,9 +6,21 @@ Vue.createApp({
             lastName: 'Savian'
         }
     }
+    //# id selector
+    //. class selector
 }).mount('#app')
 //what element it controls
 //expressions: evaluates to a single value, only expressions are allowed on the {} in the html
     //no var declarations / no code behinde de scenes
     //it should be created on the template where its mounted on --> app
     //data MUST be inside the data function
+
+Vue.createApp({
+        data() {
+            //gets called when created
+            return {
+                firstName: 'Bruno',
+                lastName: 'Rodrigues'
+            }
+        }
+}).mount('#secondApp')
